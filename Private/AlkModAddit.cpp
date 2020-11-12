199a9a23ea88eaa84b7bae78a583d9251cd8088e
Christopher Augustus <repos@c4augustus.com>
2020-11-12 20:36:39 +0000

    create minimal code required for the UE C++ module

---
+++ Private/AlkModAddit.cpp
@@ -0,0 +1,6 @@
+// Copyright 2020 Alkaline Games, LLC. All Rights Reserved.
+
+#include "AlkModAddit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_GAME_MODULE(FDefaultGameModuleImpl, AlkModAddit);
---
+++ Private/AlkModAddit.h
@@ -0,0 +1,5 @@
+// Copyright 2020 Alkaline Games, LLC. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
