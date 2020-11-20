7a571e1c992518a308f8a70270da17de8ef9a5b6
Christopher Augustus <repos@c4augustus.com>
2020-11-20 16:59:34 +0000

    improve AlkAddPersister and implement its backend REST API call

--- Private/AlkAddPersister.cpp
+++ Private/AlkAddPersister.cpp
@@ -2,17 +2,79 @@
 
 #include "AlkAddPersister.h"
 
+#include "AlkAddBackendCaller.h"
+#include "AlkAddPersistentId.h"
+
 DECLARE_LOG_CATEGORY_EXTERN(LogAlkAddPersister, Log, All);
 DEFINE_LOG_CATEGORY(LogAlkAddPersister);
 
+class UAlkAddPersister::PrivateImpl
+{
+  TWeakObjectPtr<UAlkAddBackendCaller> BackendCaller;
+public:
+  FString HostName;
+
+  UAlkAddBackendCaller& MutateBackendCaller()
+  {
+    if (!BackendCaller.IsValid())
+    {
+      BackendCaller = NewObject<UAlkAddBackendCaller>();
+      BackendCaller->AddToRoot(); // !!! prevent premature GC
+      BackendCaller->ConfigureHostName(HostName);
+    }
+    return *BackendCaller;
+  }
+  ~PrivateImpl()
+  {
+    if (BackendCaller.IsValid())
+      BackendCaller->RemoveFromRoot(); // !!! allow GC
+  }
+};
+
+// private
+UAlkAddPersister::UAlkAddPersister(
+  const class FObjectInitializer& ObjectInitializer)
+  : Super(ObjectInitializer),
+    Impl(new PrivateImpl)
+{}
+
+// virtual
+void UAlkAddPersister::BeginDestroy() // override
+{
+  delete Impl;
+  Impl = nullptr;
+  Super::BeginDestroy();
+}
+
 // static
-FString // returns PersistentId
+UAlkAddPersister*
+UAlkAddPersister::AlkAddCreatePersister(
+  FString HostName)
+{
+  auto Object = NewObject<UAlkAddPersister>();
+  Object->Impl->HostName = HostName;
+  return Object;
+}
+
+void
 UAlkAddPersister::AlkAddPersist(
-  FString InSetId, // blank uses "<company name>:<project name>"
-  FString InPersistentId, // blank generates new ID, i.e. creation
+  const FString& InSetId, // blank uses "<company name>:<project name>"
+  UAlkAddPersistentId* InOutPersistentId,
+   // ^ blank generates a new ID, i.e. creation, so this reference
+   // is held until its value is assigned at asynchronous completion
   const UActorComponent* Addit,
   const TMap<FString,FString>& NamedValues)
 {
+  if (!InOutPersistentId)
+  {
+    UE_LOG(LogAlkAddPersister, Error, TEXT("nullptr passed for PersistentId"));
+    return;
+  }
+  if (!Addit)
+  {
+    UE_LOG(LogAlkAddPersister, Error, TEXT("nullptr passed for Addit"));
+    return;
+  }
   FString SetId = InSetId;
   SetId.TrimStartAndEndInline();
   if (SetId.IsEmpty())
@@ -33,12 +95,14 @@ UAlkAddPersister::AlkAddPersist(
     );
     SetId = CompanyName + ':' + ProjectName;
   }
-  FString PersistentId = InPersistentId;
+  FString PersistentId = InOutPersistentId->Value;
   PersistentId.TrimStartAndEndInline();
-  if (PersistentId.IsEmpty())
-  {
-    PersistentId = "###PlaceholderPersistentId###";
-  }
-  // TODO: ### COMPLETE IMPLEMENTATION
-  return PersistentId;
+  const UObject* Owner = Addit->GetOwner();
+  FName ClassName = Owner ? Owner->GetClass()->GetFName() : "<null>";
+  Impl->MutateBackendCaller().RequestPersist(
+    SetId, PersistentId, ClassName.ToString(), NamedValues,
+    [InOutPersistentId] (FString PersistentId) {
+      InOutPersistentId->Value = PersistentId;
+    }
+  );
 }
--- Public/AlkAddPersister.h
+++ Public/AlkAddPersister.h
@@ -4,16 +4,29 @@
 
 #include "AlkAddPersister.generated.h"
 
-UCLASS()
-class UAlkAddPersister : public UObject
+class UAlkAddPersistentId;
+
+UCLASS(BlueprintType, Transient)
+class ALKMODADDIT_API UAlkAddPersister : public UObject
 {
+  class PrivateImpl;
+  PrivateImpl* Impl = nullptr;
   GENERATED_BODY()
+  UAlkAddPersister(const class FObjectInitializer& ObjectInitializer);
 public:
+  virtual void BeginDestroy() override;
+
   UFUNCTION(BlueprintCallable, Category = "A L K A L I N E")
-  static FString AlkAddPersist( // returns PersitentId
-      FString SetId, // must be non-blank
-      FString PersistentId, // blank generates new ID, i.e. creation
-      const UActorComponent* Addit,
-      const TMap<FString,FString>& NamedValues
+  static UAlkAddPersister* AlkAddCreatePersister(
+    FString HostName);
+
+  UFUNCTION(BlueprintCallable, Category = "A L K A L I N E")
+  void AlkAddPersist(
+    const FString& SetId, // blank uses "<company name>:<project name>"
+    UAlkAddPersistentId* PersistentId,
+     // ^ blank generates a new ID, i.e. creation, so this reference
+     // is held until its value is assigned at asynchronous completion
+    const UActorComponent* Addit,
+    const TMap<FString,FString>& NamedValues
   );
 };
